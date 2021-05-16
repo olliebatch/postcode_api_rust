98d6ce9cb45f92f32a3c145217de4a5cf23bd3bb
olliebatch <47030523+olliebatch@users.noreply.github.com>
2021-05-16 11:47:43 +0100

    add basic postcode loc testing

--- src/api/handlers.rs
+++ src/api/handlers.rs
@@ -11,11 +11,14 @@ pub async fn handle_health_check(_: Request<state::State>) -> tide::Result<Respo
 pub async fn get_location_info(req: Request<state::State>) -> tide::Result<Response> {
     let postcode = req.param("postcode");
 
+    let postcode_api = req.state().clone().postcode_api()?;
+
     if let Ok(input_postcode) = postcode {
         let space_correction = input_postcode.replace("%20", "");
         let postcode = Postcode::new(space_correction, None);
+        let postcode_with_loc = postcode.with_location(postcode_api).await?;
         let mut res = Response::new(StatusCode::Ok);
-        res.set_body(serde_json::to_string(&postcode)?);
+        res.set_body(serde_json::to_string(&postcode_with_loc)?);
         res.append_header("Content-Type", "application/json");
         return Ok(res);
     }
--- src/postcode/mod.rs
+++ src/postcode/mod.rs
@@ -40,3 +40,38 @@ impl From<PostCodeDetails> for Postcode {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::postcode::Postcode;
+    use crate::postcode_api::api_client::PostcodeApiClient;
+    use httpmock::MockServer;
+    use serde_json::json;
+
+    #[async_std::test]
+    async fn with_location() {
+        // This would be nicer to mock the trait of the profile api client.
+        // For the essence of time i've left this as the same code for profile client can work here.
+
+        let postcode = "WC2N 5DU";
+        // Arrange
+        let server = MockServer::start();
+        let mock = server.mock(|when, then| {
+            when.method("GET").path_contains("/postcodes");
+            then.status(200)
+                .json_body(json!({ "status": 200, "result": {
+                    "postcode": "WC2N 5DU",
+                    "longitude": -0.128294,
+                    "latitude": 51.507209,
+                } }));
+        });
+        let client = PostcodeApiClient::new(&server.base_url()).unwrap();
+
+        // Act
+        let postcode_struct = Postcode::new(postcode.to_owned(), None);
+
+        let postcode_with_loc = postcode_struct.with_location(client).await.unwrap();
+
+        insta::assert_json_snapshot!(postcode_with_loc)
+    }
+}
